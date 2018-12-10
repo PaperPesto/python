7bf9733f5bac601fc57ddb92a8e703680b045f0d
Leonardo Bianchini <l.bianchini@aeffegroup.biz>
2018-12-10 18:14:05 +0100

    Corretto e funzionante; il problema era che non mettevo sleep dopo led.on()

--- web/led-server/LEDManager.py
+++ web/led-server/LEDManager.py
@@ -1,38 +1,48 @@
+# Non è possibile accendere indefinitamente un led con gpiozero
+# Ad esempio lo accendo, ritorno il controllo al controller e aspetto un altro handler, lo spengo, ecc
+# posso solo accenderlo per tot secondi, spengerlo e poi ritornare al controller
+
 from gpiozero import LED
 from time import sleep
 
-led=LED(26)
 
-
-def on(ledpin):
-    print("method on on led", ledpin)
-    #led = LED(ledpin)
-    print(led)
+def on(ledpin, delay):
+    action = "on"
+    print("Turning {0} led {1} for {2} seconds...", action, ledpin, delay)
+    led = LED(ledpin)
     led.on()
+    sleep(delay)
 
-def off(ledpin):
-    print("method off on led", ledpin)
-    #led = LED(ledpin)
-    print(led)
+
+def off(ledpin, delay):
+    action = "off"
+    print("Turning {0} led {1} for {2} seconds...", action, ledpin, delay)
+    led = LED(ledpin)
     led.off()
+    sleep(delay)
 
 
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) == 1:
-        print("Dice Manager module, type help as argument")
+        print("LEDManager module, type help as argument")
     elif len(sys.argv) > 1:
         if sys.argv[1] == 'help':
-            print("Usage: $ python led-manager.py <arg1> <arg2>")
+            print("Usage: $ python led-manager.py <arg1> <arg2> <arg3>")
+            print("<arg1>: led")
+            print("<arg2>: action")
+            print("<arg3>: delay")
         else:
+            # Argomenti passati -----
             ledpin = int(sys.argv[1])
             print("led:", str(ledpin))
             action = sys.argv[2]
             print("action:", action)
+            delay = sys.argv[3]
+            print("delay:", delay)
+
             if action == 'on':
-                print("Executing action on...")
-                on(ledpin)
+                on(ledpin, delay)
             elif action == 'off':
-                print("Executing action off...")
-                off(ledpin)
+                off(ledpin, delay)
