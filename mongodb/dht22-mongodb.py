fa08a7f0d7717277241d8274d9983dd437582fea
Leonardo Bianchini <l.bianchini@aeffegroup.biz>
2018-12-18 16:05:53 +0100

    Completato script per lettura temperatura e umidità ed inserimento su mongodb

--- mongodb/dht22-mongodb.py
+++ mongodb/dht22-mongodb.py
@@ -3,6 +3,7 @@
 
 from pymongo import MongoClient
 import Adafruit_DHT as dht
+import datetime
 
 client = MongoClient('localhost', 27017)
 
@@ -15,14 +16,13 @@ collection = db.test_collection
 while True:
     humidity,temperature = dht.read_retry(dht.DHT22, 21)
 
-    print(temperature)
+    print(temperature, humidity)
 
-# Creo un documento in JSON da inserire nella collection
-document = {
-    "name": "leonardo",
-    "age": 28
-}
+    document = {
+        "room": "Aeffegroup office",
+        "temperature": temperature,
+        "humidity": humidity,
+        "date": datetime.datetime.utcnow()
+    }
 
-# Inserisco il documento nella collection e mi faccio restituire l'id
-document_id = collection.insert_one(document).inserted_id
-print("Id oggetto inserito", document_id)
+    document_id = collection.insert_one(document).inserted_id
