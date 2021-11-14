2001195a2048dd9238ae117811c885e7b1d1e07f
Leonardo Bianchini <sbwnco@gmail.com>
2021-11-14 13:29:58 +0100

    aggiunte distribuzioni non uniformi e quadratica

--- noise/rnd-uniform-bars.py
+++ noise/rnd-uniform-bars.py
@@ -1,36 +1,47 @@
 import random
 import matplotlib.pyplot as plt
 
+# distribuzione uniforme
 def gen():
     map = [0] * 20
-    pos = random.randint(0,19)
+    pos = random.randint(0, 19)
     map[pos] = 1
     return map
 
+# distribuzione quadratica
+def gen_sqr():
+    map = [0] * 20
+    pos = random.randint(0, 19)
+    pos = int(pos*pos/19)
+    map[pos] = 1
+    return map
+
+# distribuzione non uniforme
 def gen_array(rnd, div):
     map = [0] * 20
     pos = rnd // div
     map[pos] = 1
     return map
 
-axislimits = [0,20,0,4]
-width=1
+
+axislimits = [0, 20, 0, 4]
+width = 1
 
 fig, axs = plt.subplots(4)
 
 # Set the ticks and ticklabels for all axes
 # plt.setp(axs, xticks=[0.1, 0.5, 0.9], xticklabels=['a', 'b', 'c'], yticks=[1, 2, 3])
 
-#decommentare per partire da una distribuzione flat per vedere 4 distribuzioni diverse
+# decommentare per partire da una distribuzione flat per vedere 4 distribuzioni diverse
 # rnd = random.randint(0,19)
 # bars0 = gen_array(rnd, 1)
 # bars1 = gen_array(rnd, 2)
 # bars2 = gen_array(rnd, 3)
 # bars3 = gen_array(rnd, 4)
-bars0 = gen()
-bars1 = gen()
-bars2 = gen()
-bars3 = gen()
+bars0 = gen_sqr()
+bars1 = gen_sqr()
+bars2 = gen_sqr()
+bars3 = gen_sqr()
 axs[0].bar(range(len(bars0)), bars0, align='edge', width=width, color='red')
 axs[1].bar(range(len(bars1)), bars1, align='edge', width=width, color='orange')
 axs[2].bar(range(len(bars2)), bars2, align='edge', width=width, color='green')
