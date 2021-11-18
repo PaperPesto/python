565997ae7e6439fa95c88a52fd4abc8140017633
Leonardo Bianchini <sbwnco@gmail.com>
2021-11-18 23:05:46 +0100

    Rinormalizzazione tra 0 e 255

--- noise/perlin-noise/perlin-noise.py
+++ noise/perlin-noise/perlin-noise.py
@@ -3,10 +3,15 @@
 
 import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
+import numpy as np
 
 noise = PerlinNoise(octaves=10, seed=1)
 xpix, ypix = 100, 100
 pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
 
-plt.imshow(pic, cmap='gray')
+pic = np.array(pic)
+pic = np.floor((pic + 0.5) * 255)   # rinormalizzato tra 0 e 255
+
+plt.imshow(pic, cmap='Blues')
+plt.colorbar()
 plt.show()
