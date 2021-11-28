ba2102bcae14a79e68d2e41c91e9f36e12d6ebeb
Leonardo Bianchini <sbwnco@gmail.com>
2021-11-28 14:05:47 +0100

    Cliccando col mouse si possono inserire nuovi fiumi; il fattore di rescale adesso è 1024 perché non ho ancora capito il codominio della funzione di perlin noise

--- noise/noise/noise-scipy-export.py
+++ noise/noise/noise-scipy-export.py
@@ -1,10 +1,12 @@
+# https://medium.com/@yvanscher/playing-with-perlin-noise-generating-realistic-archipelagos-b59f004d8401
+
 import noise
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-shape = (32,32)
-scale = 100.0
+shape = (256,256)
+scale = 300.0
 octaves = 6
 persistence = 0.5
 lacunarity = 2.0
@@ -25,7 +27,7 @@ content = ""
 
 for i in range(shape[0]):
     for j in range(shape[1]):
-        content = content + str(math.floor(world[i][j] * 256)) + ","
+        content = content + str(math.floor(world[i][j] * 1024)) + ","
     
     content += "\n"
 
